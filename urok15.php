d26c94e4e82d4bad69d62c2369ad4a40d34db831
HP <darjasalnichenko4@gmail.com>
2021-07-15 20:42:24 +0300

    классы и обьекты

--- urok15.php
+++ urok15.php
@@ -4,7 +4,7 @@
 class animal
 {
     public $name = 'Unknown';
-    public $firstname = 'Unknown';
+    public $color = 'Unknown';
     public $age = '..';
     public $gender = 'Unknown';
 
@@ -14,12 +14,12 @@ $a = new animal();
 $b = new animal();
 
 $a->name='Bruno';
-$a->firstname='Kalzer';
+$a->color='gray with black stripes';
 $a->gender='Man';
 $a->age=2.5;
 
 $b->name='Hvostik';
-$b->firstname='Pushochkin';
+$b->color='white with black spots';
 $b->age=1;
 $b->gender='Man';
 
